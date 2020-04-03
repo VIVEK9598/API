aa86ccfd5365fee96a6895aaff1027bb2b0884cc
Komal Mishra <komalmishra958@gmail.com>
2020-04-03 18:29:16 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+
+  state = {
+    post: []
+  }
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`)
+      .then(res => {
+        this.setState({ post: res.data })
+
+
+      })
+
+  }
   render() {
+
+    const { post } = this.state
+
+    const mystyle = {
+      border: "1px solid black"
+    };
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
+        <table style={mystyle}>
+          <thead>
+            <tr style={mystyle}>
+              <th style={mystyle}>userId </th>
+              <th style={mystyle}>Id</th>
+              <th style={mystyle}>Title</th>
+              <th style={mystyle}>Body</th>
+              <th style={mystyle}>Action</th>
+            </tr>
+          </thead>
+          <tbody>{post.map(p => {
+            return (<tr key={p.id}>
+
+              <td style={mystyle}>{p.userId}</td>
+              <td style={mystyle}>{p.id}</td>
+              <td style={mystyle}>{p.title}</td>
+              <td style={mystyle}>{p.body}</td>
+
+            </tr>
+            )
+          })
+          }
+          </tbody>
+
+
+        </table>
       </div>
     );
   }
